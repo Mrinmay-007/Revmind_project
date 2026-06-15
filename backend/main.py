036b38bc5bc99228de2e9ecf496d5189c8a26d0c
mrinmay2004 <mrinmay21012204@gmail.com>
2026-06-15 23:40:16 +0530

    update router for chat

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ from fastapi.middleware.cors import CORSMiddleware
 # ============================
 
 from seed import seed_db
-from api_router import check,products
+from api_router import check,products,chat
 
 # ============================
 
@@ -39,6 +39,7 @@ def startup():
 
 app.include_router(check.router)
 app.include_router(products.router)
+app.include_router(chat.router)
 
 
 
